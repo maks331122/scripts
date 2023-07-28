1b59b120532c39083bfcec9f0ddb2e7cfa086fa8
maskhav <60893132+maks331122@users.noreply.github.com>
2023-07-28 09:42:41 +0300

    +apache2

---
+++ scriptapache2.py
@@ -0,0 +1,23 @@
+import os
+
+dns = input('Введіть ваш DNS:')
+
+os.chdir('/etc/apache2/sites-available/')
+
+with open('{0}.conf'.format(dns.split('.')[0]), 'w') as f:
+	f.write('<VirtualHost *:80>\n')
+	f.write('ServerName {0}\n'.format(dns))
+	f.write('DocumentRoot /var/www/{0}\n'.format(dns.split('.')[0]))
+	f.write('</VirtualHost>\n')
+
+if not os.path.exists('/var/www/{0}'.format(dns.split('.')[0])):
+	os.mkdir('/var/www/{0}'.format(dns.split('.')[0]))
+
+with open('/var/www/{0}/index.html'.format(dns.split('.')[0]), 'w') as f:
+	f.write('{0}'.format(dns.split('.')[0]))
+
+os.system('a2ensite {0}.conf'.format(dns.split('.')[0]))
+os.system('systemctl restart apache2')
+
+print('Apache 2 налаштовано!!')
+
