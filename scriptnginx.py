980994f935926f38c41cb3fac5259c39c2aa436a
maskhav <60893132+maks331122@users.noreply.github.com>
2023-11-03 18:50:44 +0200

    Update scriptnginx.py

--- scriptnginx.py
+++ scriptnginx.py
@@ -11,7 +11,7 @@ with open('site.conf', 'w') as f:
 	f.write("server {\n")
 	f.write("	listen 8080;\n")
 	f.write("	listen [::]:8080;\n")
-	f.write("	root /var/www/html/index.html;\n")
+	f.write("	root /var/www/html/libro/public;\n")
 	f.write("	index index.html;\n")
 	f.write("	server_name %s;\n" % ip)
 	f.write("	location / {\n")
