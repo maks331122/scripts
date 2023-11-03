c4bb8efee52647f070f64a93c0b4d91bd8f727af
maskhav <60893132+maks331122@users.noreply.github.com>
2023-11-03 18:14:47 +0200

    Update scriptnginx.py

--- scriptnginx.py
+++ scriptnginx.py
@@ -11,7 +11,7 @@ with open('site.conf', 'w') as f:
 	f.write("server {\n")
 	f.write("	listen 8080;\n")
 	f.write("	listen [::]:8080;\n")
-	f.write("	root /var/www/site/index.html;\n")
+	f.write("	root /var/www/html/index.html;\n")
 	f.write("	index index.html;\n")
 	f.write("	server_name %s;\n" % ip)
 	f.write("	location / {\n")
@@ -19,9 +19,8 @@ with open('site.conf', 'w') as f:
 	f.write("	}\n")
 	f.write("}\n")
 
-if not os.path.exists('/var/www/site'):
-	os.mkdir('/var/www/site')
-	with open('/var/www/site/index.html', 'w') as f:
+if not os.path.exists('/var/www/html'):
+	with open('/var/www/html/index.html', 'w') as f:
 		f.write('site{0}'.format(instance))
 
 os.system('ln -n site.conf /etc/nginx/sites-enabled/site.conf')
